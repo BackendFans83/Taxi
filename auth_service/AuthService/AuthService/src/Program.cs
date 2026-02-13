3aef6056b10760c94de7bafae9e806d8ded6d86d
rvfw <k.daniil2506@gmail.com>
2026-02-13 17:04:12 +0400

    feat(auth): add DI and middlewares for routing and cors

--- auth_service/AuthService/AuthService/src/Program.cs
+++ auth_service/AuthService/AuthService/src/Program.cs
@@ -1,6 +1,25 @@
+using AuthService.Clients;
+using AuthService.Repositories;
+using AuthService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IAuthService,AuthService.Services.AuthService>();
+builder.Services.AddScoped<IOAuthService,OAuthService>();
+builder.Services.AddScoped<IAuthRepository,AuthRepository>();
+builder.Services.AddScoped<ICacheRepository,RedisCacheRepository>();
+
+builder.Services.AddHttpClient<GoogleOAuthClient>();
+builder.Services.AddHttpClient<AppleOAuthClient>();
+builder.Services.AddScoped<IOAuthClient>(sp=>sp.GetRequiredService<GoogleOAuthClient>());
+builder.Services.AddScoped<IOAuthClient>(sp=>sp.GetRequiredService<AppleOAuthClient>());
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(p=>p.WithOrigins(builder.Configuration["Client:Url"]).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+app.MapControllers();
 
 app.Run();
