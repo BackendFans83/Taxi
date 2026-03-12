6c51780c72376b4c3c22d48f6c485ba7f47360ca
Kusamus <diogenosinopefs@gmail.com>
2026-03-12 21:59:41 +0400

    Add DbContext and message model

--- NotificationService/API/Program.cs
+++ NotificationService/API/Program.cs
@@ -1,3 +1,9 @@
+using API.ExtensionMethods;
+using Domain.Enums;
+using DotNetEnv;
+using Infrastructure;
+using Infrastructure.Entities;
+
 namespace API;
 
 public class Program
@@ -5,17 +11,17 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        Env.Load();
+        builder.Services.AddNotificationsDbContext();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        
-        
         var app = builder.Build();
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
+        
         app.Run();
     }
 }
