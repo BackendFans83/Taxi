46ad0a975aaccce503de423de51fe1f65a6925c2
Kusamus <diogenosinopefs@gmail.com>
2026-03-13 21:13:07 +0400

    replacing enum-topics with a string

--- NotificationService/Domain/Enums/NotificationTopics.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Enums;
-
-public enum NotificationTopics
-{
-    Payment,
-    Promotion,
-    Driver   // сюда сообщения по типу "машина приехала", "водитель найден", "водитель отказался от заказа"
-             // буду очень рад, если подберете более подходящее слово
-}
