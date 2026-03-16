7f052b3e0f6f529fd1db6d60b7a48ad097e2493f
rvfw <k.daniil2506@gmail.com>
2026-03-16 19:51:36 +0400

    feat(ride): add di and routing

--- RideService/RideService/src/Program.cs
+++ RideService/RideService/src/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using RideService.Data;
+using RideService.Repositories;
+using RideService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IRideService, RideService.Services.RideService>();
+builder.Services.AddScoped<IRideRepository, RideRepository>();
 
 #region db_connections
 
@@ -20,5 +26,9 @@ var app = builder.Build();
 using var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 await db.Database.OpenConnectionAsync();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
