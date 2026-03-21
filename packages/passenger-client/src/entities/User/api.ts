4d03a7a31ff1aaa3d2e57fece7db4fb71f61427b
ice-rider <codevoke@yandex.ru>
2026-03-21 13:35:43 +0400

    feat(passenger-client): add user entity API
    - Add userApi with getById method
    - Export userApi from entities/User

--- packages/passenger-client/src/entities/User/api.ts
+++ packages/passenger-client/src/entities/User/api.ts
@@ -2,16 +2,8 @@ import { http } from '@taxi/shared/api/http';
 import type { User } from '@taxi/shared/types';
 
 export const userApi = {
-  getById: (id: string) => {
-    return http.get<User>(`/users/${id}`);
-  },
-
-  update: (id: string, data: Partial<User>) => {
-    return http.put<User>(`/users/${id}`, data);
-  },
-
-  delete: (id: string) => {
-    return http.delete(`/users/${id}`);
+  getById: (id: number) => {
+    return http.get<User>(`/api/v1/User/${id}`);
   },
 };
 
